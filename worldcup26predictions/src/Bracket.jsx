488e1c38732d32f11a092f12dca8c1c878e4f4e9
SebC750 <SebC89999@gmail.com>
2024-08-17 01:12:47 -0400

    fixing bugs

--- worldcup26predictions/src/Bracket.jsx
+++ worldcup26predictions/src/Bracket.jsx
@@ -1,14 +1,14 @@
 import "./Bracket.css"
 import countries from './teamflag.json'
-import { useState } from "react"
+import { useState, useEffect } from "react"
 const Bracket = ({ groupname, titlecolor, teams }) => {
-   
+
     const [teamList, updateTeams] = useState(teams)
     const [selectedTeam, setSelectedTeam] = useState(null)
     const [num_of_teams, updateNum_of_teams] = useState(0)
 
-    const addTeam = () =>{
-        
+    const addTeam = () => {
+
         if (selectedTeam && !teamList.includes(selectedTeam.country)) {
             const newTeamList = [...teamList, selectedTeam.country];
             updateTeams(newTeamList);
@@ -29,58 +29,48 @@ const Bracket = ({ groupname, titlecolor, teams }) => {
                 </div>
 
                 <div className="bracket-team">
-                    <div className="bracket-table">
+                <div className="bracket-table">
+    <table className="table">
+        <thead>
+            <tr>
+                <th scope="col">Team</th>
+                <th scope="col">MP</th>
+                <th scope="col">W</th>
+                <th scope="col">L</th>
+                <th scope="col">D</th>
+                <th scope="col">GA</th>
+                <th scope="col">GF</th>
+                <th scope="col">GD</th>
+                <th scope="col">PTS</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            {teamList.map((val, index) => (
+                <tr key={index}>
+                    <td>
+                        <span className={`fi fi-${val.toLowerCase().replace(' ', '-')}`} style={{fontSize: "2em"}}></span>
+                    </td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>0</td>
+                    <td>
+                        <button className="btn btn-danger" onClick={() => removeCountry(val)}>Remove</button>
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+    </table>
+    {num_of_teams < 4 && (
+        <button className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#add-country-modal">Add team</button>
+    )}
+</div>
 
-
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Team</th>
-                                    <th scope="col">MP</th>
-                                    <th scope="col">W</th>
-                                    <th scope="col">L</th>
-                                    <th scope="col">D</th>
-                                    <th scope="col">GA</th>
-                                    <th scope="col">GF</th>
-                                    <th scope="col">GD</th>
-                                    <th scope="col">PTS</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-
-                                {teamList.map((val, index) => (
-
-                                    <tr key={index}>
-
-                                        <td> <span className={`fi fi-${val.toLowerCase().replace(' ', '-')}`} style={{fontSize: "2em"}}></span></td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td>0</td>
-                                        <td> <div className="btn btn-danger" onClick={() => removeCountry(val)}> Remove </div></td>
-                                    </tr>
-
-                                ))
-
-                                }
-
-
-
-
-
-                                {num_of_teams < 4? (
-                                    <div className="btn btn-danger" data-bs-toggle="modal" data-bs-target="#add-country-modal"> Add team</div>
-                                ) : null}
-                            </tbody>
-                        </table>
-
-
-                    </div>
                 </div>
                 <div className="modal fade" id="add-country-modal" tabIndex="-1" aria-labelledby="add-country-label" aria-hidden="true">
                     <div className="modal-dialog">
@@ -90,16 +80,16 @@ const Bracket = ({ groupname, titlecolor, teams }) => {
 
                             </div>
                             <div className="modal-body">
-                                { selectedTeam ? (
-                                    <div className="selected-country-display"> <span className={`fi fi-${selectedTeam.country.toLowerCase().replace(' ', '-')}`} style={{fontSize: "2em"}}> </span>
+                                {selectedTeam ? (
+                                    <div className="selected-country-display"> <span className={`fi fi-${selectedTeam.country.toLowerCase().replace(' ', '-')}`} style={{ fontSize: "2em" }}> </span>
                                         <h3> {selectedTeam.name} </h3> </div>
-                                ):null}
-                                
+                                ) : null}
+
                                 <div className="selection-window">
                                     {countries.map((val, index) =>
-                                        <div className="country-option" key={index} onClick={() => setSelectedTeam({country: val.country, name: val.name})}>
+                                        <div className="country-option" key={index} onClick={() => setSelectedTeam({ country: val.country, name: val.name })}>
                                             <span> {val.name} </span>
-                                            <span className={`fi fi-${val.country.toLowerCase().replace(' ', '-')}`} style={{fontSize: '2em'}}> </span>
+                                            <span className={`fi fi-${val.country.toLowerCase().replace(' ', '-')}`} style={{ fontSize: '2em' }}> </span>
                                         </div>
                                     )}
 
